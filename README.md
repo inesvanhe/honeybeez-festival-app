148de430eff01a85eda841f3d0b1655e5283b881
Ines <vanheckeines@gmx.de>
2026-06-12 19:12:04 +0200

    Setup project structure

---
+++ README.md
@@ -0,0 +1,3 @@
+# HONEY|BEEZ Festival App
+
+Eine fiktive Festival-App für HONEY|BEEZ, entwickelt im Rahmen einer React-Aufgabe.
---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <main>
+      <h1>HONEY|BEEZ Festival-App</h1>
+    </main>
+  );
+}
+
+export default App;
